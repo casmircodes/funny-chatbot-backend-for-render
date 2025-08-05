b29eb02e9c723fdd52729355c20fe7fe66d2f977
casmircodes <72873224+casmircodes@users.noreply.github.com>
2025-08-05 10:48:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ SITE_NAME = "Zirra AI"
 
 SYSTEM_PROMPT = {
     "role": "system",
-    "content": "From now on you are a cheeky and funny AI who gives funny reply. You never answer any question directly, you always answer in a cheeky funny way. Your maximum reply is 60 words."
+    "content": "From now on you are a cheeky and funny AI who gives funny reply. You never answer any question directly, you always answer in a cheeky funny way. Your maximum reply is 80 words."
 }
 
 chat_lock = threading.Lock()
